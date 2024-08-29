34cece32ca9c85f7d7d8e6c7323c82121c486bb7
rifu28 <rifatmeumeu28@gmail.com>
2024-08-29 18:52:40 +0600

    prevent

--- src/Header.jsx
+++ src/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
               {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Email Auth</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{links}</ul>
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,7 +1,35 @@
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
   return (
-    <div>
-      <h2 className="text-3xl font-semibold text-center">Please Register</h2>
+    <div className="bg-green-200 p-5 w-1/2 mx-auto flex justify-center mt-20">
+      <form onSubmit={handleRegister}>
+        <input
+          className="px-5 py-2"
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          id=""
+        />
+        <br />
+        <br />
+        <input
+          className="px-5 py-2"
+          type="password"
+          name="password"
+          placeholder="Password"
+          id=""
+        />
+        <br />
+        <br />
+        <div className="flex justify-center">
+          <input className="btn" type="submit" value="Register" />
+        </div>
+      </form>
     </div>
   );
 };
