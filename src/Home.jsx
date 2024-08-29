5aff309b6a6b8ffc948b5a962740540be2d01b29
rifu28 <rifatmeumeu28@gmail.com>
2024-08-29 18:21:30 +0600

    header

---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>THis is HOMe</h2>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
