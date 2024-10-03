5e288f0974377d99f3675bf3681dac884ff3f3c3
rifu28 <rifatmeumeu28@gmail.com>
2024-10-03 16:36:31 +0600

    with function

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,19 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import auth from "./firebase.init";
 
-// const Login = () => {
-//   const handleLogin = (e) => {
-//     e.preventDefault();
-//     const email = e.target.email.value;
-//     const password = e.target.password.value;
-//     signInWithEmailAndPassword(auth, email, password)
-//       .then((result) => {
-//         console.log(result.user);
-//       })
-//       .then((error) => {
-//         console.log(error.message);
-//       });
-//   };
+const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .then((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <div>
       <h2 className="text-3xl font-semibold text-center mt-20">Login Here</h2>
